bcefc4b90ae3a24624c90778de8e2e56fd61b5e3
John Carlo Lipa <johncarlolipa@gmail.com>
2024-02-20 21:35:59 +0800

    added get all users routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -38,4 +38,14 @@ router.post("/login", async (req, res) => {
   }
 });
 
+// Get all users
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
