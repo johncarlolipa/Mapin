1ccf8f92d9e93e1736733e4c07eb39c1282101ad
John Carlo Lipa <johncarlolipa@gmail.com>
2024-02-20 21:31:44 +0800

    added hello and 404 error

--- server/index.js
+++ server/index.js
@@ -2,11 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./connectDB");
 const app = express();
-const pinRoute = require("./routes/pins")
-const userRoute = require("./routes/users")
+const pinRoute = require("./routes/pins");
+const userRoute = require("./routes/users");
 
 app.use(express.json());
-
 
 // middleware
 connectDB();
@@ -15,6 +14,13 @@ connectDB();
 app.use("/api/pins", pinRoute);
 app.use("/api/users", userRoute);
 
+app.get("/", (req, res) => {
+  res.json("hello");
+});
+
+app.get("*", (req, res) => {
+  res.json("404");
+});
 
 app.listen(8000, () => {
   console.log("server is running");
