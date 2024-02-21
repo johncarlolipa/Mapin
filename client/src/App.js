3d065f24057c7254335810de262712d562403acd
John Carlo Lipa <johncarlolipa@gmail.com>
2024-02-21 18:38:04 +0800

    add mapbox react

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
-
+import * as React from "react";
+import Map from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <Map
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+      initialViewState={{
+        longitude: 46,
+        latitude: 17,
+        zoom: 4,
+      }}
+      style={{ width: "98vw", height: 1000 }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    />
+  );
 }
 
 export default App;
