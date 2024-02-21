21e005eb1bf9d7c73934da16b7d6c6646c2bb0c9
John Carlo Lipa <johncarlolipa@gmail.com>
2024-02-21 22:05:29 +0800

    fetch data using axios

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,29 @@ import Map, { Marker, Popup } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import FmdGoodIcon from "@mui/icons-material/FmdGood";
 import Star from "@mui/icons-material/Stars";
+import axios from "axios";
+import { format } from "timeago.js";
 
 function App() {
-  // const [showPopup, setShowPopup] = useState(true);
+  const currentUser = "jc low"
+  const [pins, setPins] = React.useState([]);
+  const [currentPlaceId, setCurrentPlaceId] = React.useState(null);
+
+  React.useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get("/pins");
+        setPins(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getPins();
+  }, []);
+
+  const handleMarkerClick = (id) => {
+    setCurrentPlaceId(id);
+  };
 
   return (
     <Map
@@ -18,37 +38,46 @@ function App() {
       style={{ width: "98vw", height: 1000 }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
     >
-      <Marker longitude={3} latitude={40} anchor="bottom">
-        <FmdGoodIcon
-          style={{ fontSize: visualViewport.zoom * 7, color: "slateblue" }}
-        />
-      </Marker>
-      {/* {showPopup && ( */}
-      <Popup
-        longitude={46}
-        latitude={17}
-        anchor="left"
-        closeButton={true}
-        closeOnClick={false}
-      >
-        <div>
-          <label>Place</label>
-          <h4>Eifell</h4>
-          <label>Review</label>
-          <p>Beuatiful Place</p>
-          <label>Rating</label>
-          <div>
-            <Star />
-            <Star />
-            <Star />
-            <Star />
-          </div>
-          <label>Information</label>
-          <span>Created by <b>JC</b></span>
-          <span>1 hour ago</span>
-        </div>
-      </Popup>
-      {/* )} */}
+      {pins.map((pin, index) => (
+        <React.Fragment key={index}>
+          <Marker longitude={pin.long} latitude={pin.lat} anchor="bottom">
+            <FmdGoodIcon
+              style={{ fontSize: visualViewport.zoom * 7, color: pin.username === currentUser ? "tomato" : "slateblue" }}
+              onClick={() => handleMarkerClick(pin._id)}
+              className="cursor-pointer"
+            />
+          </Marker>
+          {pin._id === currentPlaceId && (
+            <Popup
+              longitude={pin.long}
+              latitude={pin.lat}
+              anchor="left"
+              closeButton={true}
+              closeOnClick={false}
+              onClose={() => setCurrentPlaceId(null)}
+            >
+              <div>
+                <label>Place</label>
+                <h4>{pin.title}</h4>
+                <label>Review</label>
+                <p>{pin.description}</p>
+                <label>Rating</label>
+                <div>
+                  <Star />
+                  <Star />
+                  <Star />
+                  <Star />
+                </div>
+                <label>Information</label>
+                <span>
+                  Created by <b>{pin.username}</b>
+                </span>
+                <span>{format(pin.createdAt)}</span>
+              </div>
+            </Popup>
+          )}
+        </React.Fragment>
+      ))}
     </Map>
   );
 }
