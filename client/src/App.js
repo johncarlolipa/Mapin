ca4834ac1f98e191044b5c531a69ce47f0768172
John Carlo Lipa <johncarlolipa@gmail.com>
2024-02-21 19:48:51 +0800

    added popup and marker

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import * as React from "react";
-import Map from "react-map-gl";
+import Map, { Marker, Popup } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import FmdGoodIcon from "@mui/icons-material/FmdGood";
+import Star from "@mui/icons-material/Stars";
 
 function App() {
+  // const [showPopup, setShowPopup] = useState(true);
+
   return (
     <Map
       mapboxAccessToken={process.env.REACT_APP_MAPBOX}
@@ -13,7 +17,39 @@ function App() {
       }}
       style={{ width: "98vw", height: 1000 }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
-    />
+    >
+      <Marker longitude={3} latitude={40} anchor="bottom">
+        <FmdGoodIcon
+          style={{ fontSize: visualViewport.zoom * 7, color: "slateblue" }}
+        />
+      </Marker>
+      {/* {showPopup && ( */}
+      <Popup
+        longitude={46}
+        latitude={17}
+        anchor="left"
+        closeButton={true}
+        closeOnClick={false}
+      >
+        <div>
+          <label>Place</label>
+          <h4>Eifell</h4>
+          <label>Review</label>
+          <p>Beuatiful Place</p>
+          <label>Rating</label>
+          <div>
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+          </div>
+          <label>Information</label>
+          <span>Created by <b>JC</b></span>
+          <span>1 hour ago</span>
+        </div>
+      </Popup>
+      {/* )} */}
+    </Map>
   );
 }
 
