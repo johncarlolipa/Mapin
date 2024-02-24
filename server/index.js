efac69c64ec419b5a68f8e034bde908a51996db5
John Carlo Lipa <johncarlolipa@gmail.com>
2024-02-24 10:49:21 +0800

    delete cors

--- server/index.js
+++ server/index.js
@@ -1,21 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./connectDB");
-const cors = require("cors");
 const app = express();
 const pinRoute = require("./routes/pins");
 const userRoute = require("./routes/users");
 
 app.use(express.json());
 
-// cors
-app.use(
-  cors({
-    origin: ["https://mapin-p829.vercel.app"],
-    methods: ["POST", "GET", "DELETE", "PUT"],
-    credentials: true,
-  })
-);
+
 
 // middleware
 connectDB();
